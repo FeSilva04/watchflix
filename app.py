d6a8244ba8c35660a243f200f6eca65116c90937
FeSilva04 <fsilva0004@gmail.com>
2025-06-21 12:32:46 -0300

    Adiciona rota e função para página de filmes

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from tmdb import get_filmes_populares
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    filmes_populares = get_filmes_populares()
-    return render_template('index.html', filmes_populares=filmes_populares)
+@app.route('/filmes')
+def filmes():
+    page = request.args.get('page', 1)
+    filmes_populares = get_filmes_populares(page)
+    return render_template('filmes.html', filmes=filmes_populares)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tmdb.py
+++ tmdb.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = '0621640672da05dd540952af6c06729b'
 
-def get_filmes_populares():
-    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=pt-BR"
+def get_filmes_populares(page=1):
+    url = f"https://api.themoviedb.org/3/movie/popular?api_key={API_KEY}&language=pt-BR&page={page}"
     response = requests.get(url)
     return response.json()['results']
